bd7bcb5bf654089422f3baf3edf759a0f24a9dc9
Daiyan Abrar Samin <2150864@edu.vaniercollege.qc.ca>
2026-09-03 15:22:57 -0400

    feat: add user input prompt for addition

--- calc.py
+++ calc.py
@@ -1,3 +1,8 @@
 print("Welcome to Calculator")
 
 def add(a, b): return a + b
+
+number1 = int(input("Enter your first number"))
+number2 = int(input("Enter your second number"))
+
+add(number1, number2)
